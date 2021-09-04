a6b7571688bf69dbac438da70c579ce82d72a22f
nazeem <nazeemnob18@gmail.com>
2021-09-04 10:22:10 +0530

    yeS

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../components/Common/Layout";
 import Body from "../components/Home/Body";
+import Status from "../components/Home/Status";
 export default function Home() {
   return (
     <Layout>
+      <Status />
       <Body />
     </Layout>
   );
